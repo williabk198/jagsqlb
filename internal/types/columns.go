7644959eb3b906aadca1184f9cc594e326afd5e0
Brandon Williams <brandonkw27@gmail.com>
2025-05-14 14:06:58 -0400

    implemented Column functions

--- internal/types/columns.go
+++ internal/types/columns.go
@@ -1,12 +1,26 @@
 package intypes
 
+import "strings"
+
 type Column struct {
 	Name  string
 	Table *Table
 }
 
 func (c Column) String() string {
-	panic("unimplmented")
+	sb := new(strings.Builder)
+
+	if c.Table != nil {
+		// Don't use Table's String method since that can return its alias which is unwanted here.
+		sb.WriteString(c.Table.ReferenceString())
+		sb.WriteRune('.')
+	}
+
+	sb.WriteRune('"')
+	sb.WriteString(c.Name)
+	sb.WriteRune('"')
+
+	return sb.String()
 }
 
 type SelectorColumn struct {
@@ -15,5 +29,16 @@ type SelectorColumn struct {
 }
 
 func (sc SelectorColumn) String() string {
-	panic("unimplmented")
+	result := sc.Column.String()
+	if sc.Alias == "" {
+		return result
+	}
+
+	sb := new(strings.Builder)
+	sb.WriteString(result)
+	sb.WriteString(` AS "`)
+	sb.WriteString(sc.Alias)
+	sb.WriteRune('"')
+
+	return sb.String()
 }
