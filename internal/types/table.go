17d2d0d5a6cd639c4a1bbbc1dbe5c5c134603c31
Brandon Williams <brandonkw27@gmail.com>
2025-05-14 13:06:44 -0400

    added ReferenceString method stub and tests for Table
    Having this method will make things easier to link columns to tables

--- internal/types/table.go
+++ internal/types/table.go
@@ -6,6 +6,14 @@ type Table struct {
 	Schema string
 }
 
+// ReferenceString returns a string that can be used as a reference to the table.
+// If the table has an alias, then this returns said alias. If the table has a schema defined,
+// then the both the schema and the table name will be returned in the format "schema.table".
+// Otherwise, just the Name field is returned.
+func (t Table) ReferenceString() string {
+	panic("unimplemented")
+}
+
 func (t Table) String() string {
 	panic("unimplemented")
 }
