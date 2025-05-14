86e09dd7afb0b50907096b0de9e1891e2c0a9cbc
Brandon Williams <brandonkw27@gmail.com>
2025-05-14 13:53:46 -0400

    implemented Table functions

--- internal/types/table.go
+++ internal/types/table.go
@@ -1,5 +1,10 @@
 package intypes
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Table struct {
 	Alias  string
 	Name   string
@@ -11,9 +16,37 @@ type Table struct {
 // then the both the schema and the table name will be returned in the format "schema.table".
 // Otherwise, just the Name field is returned.
 func (t Table) ReferenceString() string {
-	panic("unimplemented")
+	refStr := t.Name
+	if t.Alias != "" {
+		refStr = t.Alias
+	}
+
+	if t.Schema == "" || t.Alias != "" {
+		return fmt.Sprintf("%q", refStr)
+	}
+
+	return fmt.Sprintf("%q.%q", t.Schema, t.Name)
+
 }
 
 func (t Table) String() string {
-	panic("unimplemented")
+	sb := new(strings.Builder)
+
+	if t.Schema != "" {
+		sb.WriteRune('"')
+		sb.WriteString(t.Schema)
+		sb.WriteString(`".`)
+	}
+
+	sb.WriteRune('"')
+	sb.WriteString(t.Name)
+	sb.WriteRune('"')
+
+	if t.Alias != "" {
+		sb.WriteString(` AS "`)
+		sb.WriteString(t.Alias)
+		sb.WriteRune('"')
+	}
+
+	return sb.String()
 }
