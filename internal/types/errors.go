f69e1b815cde894962d1c4675630628f4c55eb89
Brandon Williams <brandonkw27@gmail.com>
2025-05-21 16:01:07 -0400

    added new error type to represent multiple errors

--- internal/types/errors.go
+++ internal/types/errors.go
@@ -1,6 +1,9 @@
 package intypes
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 var (
 	ErrMissingAliasName  = NewInvalidSytaxError("alias name not provided")
@@ -11,4 +14,26 @@ var (
 
 func NewInvalidSytaxError(errString string) error {
 	return fmt.Errorf("invalid syntax error: %s", errString)
+}
+
+type ErrorSlice []error
+
+func (es ErrorSlice) Append(err error) {
+	es = append(es, err)
+}
+
+func (es ErrorSlice) Error() string {
+	if len(es) == 0 {
+		return ""
+	}
+
+	sb := new(strings.Builder)
+	sb.WriteString(fmt.Sprintf("encountered %d error(s)", len(es)))
+
+	for _, err := range es {
+		sb.WriteString("\n\t")
+		sb.WriteString(err.Error())
+	}
+
+	return sb.String()
 }
