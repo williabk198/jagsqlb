ee91ecc91b72b7db61c47530af88847713e8f7dd
Brandon Williams <brandonkw27@gmail.com>
2025-05-28 18:00:22 -0400

    implemented functions for where clause builder

--- internal/builders/builders.go
+++ internal/builders/builders.go
@@ -1,8 +1,25 @@
 package inbuilders
 
-import "github.com/williabk198/jagsqlb/internal/utilities/parsers"
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/williabk198/jagsqlb/internal/utilities/parsers"
+)
 
 var (
 	tableParser        = parsers.NewTableParser()
 	selectColumnParser = parsers.NewSelectColumnParser()
 )
+
+// finalizeQuery replaces any "?" characters in the provided query with "$n" characters
+func finalizeQuery(query string) string {
+	pattern := regexp.MustCompile(`\?`)
+	count := 0
+	result := pattern.ReplaceAllStringFunc(query, func(value string) string {
+		count++
+		return fmt.Sprintf("$%d", count)
+	})
+
+	return result
+}
--- internal/builders/where.go
+++ internal/builders/where.go
@@ -1,6 +1,9 @@
 package inbuilders
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/williabk198/jagsqlb/builders"
 	inconds "github.com/williabk198/jagsqlb/internal/conditions"
 )
@@ -11,15 +14,65 @@ type whereBuilder struct {
 }
 
 func (w whereBuilder) Build() (query string, queryParams []any, err error) {
-	panic("unimplemented")
+	sb := new(strings.Builder)
+	mainQueryStr, params, err := w.mainQuery.Build()
+	if err != nil {
+		return "", nil, fmt.Errorf("failed to build preceding query: %w", err)
+	}
+
+	sb.WriteString(mainQueryStr[:len(mainQueryStr)-1]) // write the primary query string without the trailing ";"
+	sb.WriteString(" WHERE ")
+
+	for _, cond := range w.conditions {
+		condStr, condParams, err := cond.condition.Parameterize()
+		if err != nil {
+			return "", nil, fmt.Errorf("failed to parameterize condition %q: %w", cond, err)
+		}
+
+		params = append(params, condParams...)
+		if cond.conjunction != "" {
+			sb.WriteRune(' ')
+			sb.WriteString(cond.conjunction)
+			sb.WriteRune(' ')
+		}
+		sb.WriteString(condStr)
+	}
+	sb.WriteRune(';')
+
+	return finalizeQuery(sb.String()), params, nil
 }
 
 func (w whereBuilder) And(cond inconds.Condition, additionalConds ...inconds.Condition) builders.WhereBuilder {
-	panic("unimplemented")
+
+	w.conditions = append(w.conditions, whereCondition{
+		conjunction: "AND",
+		condition:   cond,
+	})
+
+	for _, cond := range additionalConds {
+		w.conditions = append(w.conditions, whereCondition{
+			conjunction: "AND",
+			condition:   cond,
+		})
+	}
+
+	return w
 }
 
 func (w whereBuilder) Or(cond inconds.Condition, additionalConds ...inconds.Condition) builders.WhereBuilder {
-	panic("unimplemented")
+	w.conditions = append(w.conditions, whereCondition{
+		conjunction: "OR",
+		condition:   cond,
+	})
+
+	for _, cond := range additionalConds {
+		w.conditions = append(w.conditions, whereCondition{
+			conjunction: "OR",
+			condition:   cond,
+		})
+	}
+
+	return w
 }
 
 type whereCondition struct {
