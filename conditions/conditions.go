73dfdae8f674515b6504c2d2ebe66f837879d2fa
Brandon Williams <brandonkw27@gmail.com>
2025-05-28 16:59:48 -0400

    implemented public facing condition functions

--- conditions/conditions.go
+++ conditions/conditions.go
@@ -5,49 +5,101 @@ import (
 )
 
 func Equals(columnName string, value any) inconds.Condition {
-	panic("unimplemented")
+	return inconds.SimpleCondition{
+		ColumnName: columnName,
+		Operator:   "=",
+		Values:     []any{value},
+	}
 }
 
 func NotEquals(columnName string, value any) inconds.Condition {
-	panic("unimplemented")
+	return inconds.SimpleCondition{
+		ColumnName: columnName,
+		Operator:   "!=",
+		Values:     []any{value},
+	}
 }
 
 func GraterThan(columnName string, value any) inconds.Condition {
-	panic("unimplemented")
+	return inconds.SimpleCondition{
+		ColumnName: columnName,
+		Operator:   ">",
+		Values:     []any{value},
+	}
 }
 
 func GreaterThanEqual(columnName string, value any) inconds.Condition {
-	panic("unimplemented")
+	return inconds.SimpleCondition{
+		ColumnName: columnName,
+		Operator:   ">=",
+		Values:     []any{value},
+	}
 }
 
 func LessThan(columnName string, value any) inconds.Condition {
-	panic("unimplemented")
+	return inconds.SimpleCondition{
+		ColumnName: columnName,
+		Operator:   "<",
+		Values:     []any{value},
+	}
 }
 
 func LessThanEqual(columnName string, value any) inconds.Condition {
-	panic("unimplemented")
+	return inconds.SimpleCondition{
+		ColumnName: columnName,
+		Operator:   "<=",
+		Values:     []any{value},
+	}
 }
 
 func In(columnName string, value []any) inconds.Condition {
-	panic("unimplemented")
+	return inconds.SimpleCondition{
+		ColumnName: columnName,
+		Operator:   "IN",
+		Values:     value,
+	}
 }
 
 func NotIn(columnName string, value []any) inconds.Condition {
-	panic("unimplemented")
+	return inconds.SimpleCondition{
+		ColumnName: columnName,
+		Operator:   "NOT IN",
+		Values:     value,
+	}
 }
 
 func Between(columnName string, val1, val2 any) inconds.Condition {
-	panic("unimplemented")
+	return inconds.SimpleCondition{
+		ColumnName: columnName,
+		Operator:   "BETWEEN",
+		Values:     []any{val1, val2},
+	}
 }
 
 func NotBetween(columnName string, val1, val2 any) inconds.Condition {
-	panic("unimplemented")
+	return inconds.SimpleCondition{
+		ColumnName: columnName,
+		Operator:   "NOT BETWEEN",
+		Values:     []any{val1, val2},
+	}
 }
 
 func GroupedAnd(cond1, cond2 inconds.Condition, additionalConds ...inconds.Condition) inconds.Condition {
-	panic("unimplemented")
+	conds := []inconds.Condition{cond1, cond2}
+	conds = append(conds, additionalConds...)
+
+	return inconds.GroupedConditions{
+		Conjunction: " AND ",
+		Conditions:  conds,
+	}
 }
 
 func GroupedOr(cond1, cond2 inconds.Condition, additionalConds ...inconds.Condition) inconds.Condition {
-	panic("unimplemented")
+	conds := []inconds.Condition{cond1, cond2}
+	conds = append(conds, additionalConds...)
+
+	return inconds.GroupedConditions{
+		Conjunction: " OR ",
+		Conditions:  conds,
+	}
 }
