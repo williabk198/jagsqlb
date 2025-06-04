b02eceaa8bcfe8ebb5559f55b4c1cbfd29c2075e
Brandon Williams <brandonkw27@gmail.com>
2025-06-04 16:15:52 -0400

    implemented Stringify method in ColumnOrdering

--- types/orderings.go
+++ types/orderings.go
@@ -1,10 +1,20 @@
 package types
 
+import (
+	"fmt"
+
+	"github.com/williabk198/jagsqlb/internal/utilities/parsers"
+)
+
 type ordering string
 
 const (
 	OrderingAscending  ordering = "ASC"
 	OrderingDescending ordering = "DESC"
+)
+
+var (
+	columnParser = parsers.NewColumnParser()
 )
 
 type ColumnOrdering struct {
@@ -13,5 +23,10 @@ type ColumnOrdering struct {
 }
 
 func (co ColumnOrdering) Stringify() (string, error) {
-	panic("unimplemented")
+	column, err := columnParser.Parse(co.ColumnName)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse column name %q: %w", co.ColumnName, err)
+	}
+
+	return fmt.Sprintf("%s %s", column.String(), co.Ordering), nil
 }
